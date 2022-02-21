7d5fb5c3561431ad04fbd12957c34493d0e6f782
rengas <mr.rengaswamy@gmail.com>
2022-02-21 08:53:46 +0800

    no message

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Aaruvadai Aarambam"))
+		keys, ok := r.URL.Query()["item"]
+
+		if !ok || len(keys[0]) < 0 {
+			rw.Write([]byte("Aruvadai Arambam"))
+			return
+		}
+		rw.Write([]byte(keys[0]))
 	})
 
 	http.ListenAndServe(":8080", nil)
