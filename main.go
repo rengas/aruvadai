0141b1fe915f92205561324cc514878f67dc917c
rengas <mr.rengaswamy@gmail.com>
2022-03-07 19:56:48 +0800

    Adding more info on variables.

--- main.go
+++ main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		keys, ok := r.URL.Query()["item"]
-
-		if !ok || len(keys[0]) < 0 {
-			rw.Write([]byte("Aruvadai Arambam"))
-			return
-		}
-		rw.Write([]byte(keys[0]))
-	})
-
-	fmt.Println("server started...")
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("server ending")
+	v := "Aruvadai Arambam"
+	fmt.Println(&v)
 }
